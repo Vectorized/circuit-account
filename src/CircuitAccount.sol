831920766edba8fcf7eadfa3a38aeb6adc0f289c
Vectorized <webby1111@hotmail.com>
2025-08-18 14:17:45 +0000

    Rearrange to same order as GuardedExecutor

--- src/CircuitAccount.sol
+++ src/CircuitAccount.sol
@@ -348,6 +348,16 @@ contract CircuitAccount is ERC7821, ERC1271 {
                 t.erc20s.p(target);
                 t.transferAmounts.p(LibBytes.loadCalldata(data, 0x24)); // `amount`.
             }
+            // `transferFrom(address,address,uint256)`.
+            // The account may have existing ERC20 allowances. If `transferFrom` is used
+            // to transfer to an account that is not `address(this)`, treat it as outflow.
+            if (fnSel == 0x23b872dd) {
+                // `transferFrom(address from, address to, uint256 amount)`.
+                if (LibBytes.loadCalldata(data, 0x24).lsbToAddress() == address(this)) continue;
+                if (LibBytes.loadCalldata(data, 0x44) == 0) continue; // `amount == 0`.
+                t.erc20s.p(target);
+                t.transferAmounts.p(LibBytes.loadCalldata(data, 0x44)); // `amount`.
+            }
             // `approve(address,uint256)`.
             // We have to revoke any new approvals after the batch, else a bad app can
             // leave an approval to let them drain unlimited tokens after the batch.
@@ -368,16 +378,6 @@ contract CircuitAccount is ERC7821, ERC1271 {
                 t.permit2ERC20s.p(LibBytes.loadCalldata(data, 0x04).lsbToAddress()); // `token`.
                 t.permit2Spenders.p(LibBytes.loadCalldata(data, 0x24).lsbToAddress()); // `spender`.
                 t.erc20s.p(LibBytes.loadCalldata(data, 0x04).lsbToAddress()); // `token`.
-                t.transferAmounts.p(LibBytes.loadCalldata(data, 0x44)); // `amount`.
-            }
-            // `transferFrom(address,address,uint256)`.
-            // The account may have existing ERC20 allowances. If `transferFrom` is used
-            // to transfer to an account that is not `address(this)`, treat it as outflow.
-            if (fnSel == 0x23b872dd) {
-                // `transferFrom(address from, address to, uint256 amount)`.
-                if (LibBytes.loadCalldata(data, 0x24).lsbToAddress() == address(this)) continue;
-                if (LibBytes.loadCalldata(data, 0x44) == 0) continue; // `amount == 0`.
-                t.erc20s.p(target);
                 t.transferAmounts.p(LibBytes.loadCalldata(data, 0x44)); // `amount`.
             }
         }
